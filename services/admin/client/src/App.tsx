d32ee06143f0a8f4e2cead4d2edd725b33ae28b0
Gniewomir Świechowski <gniewomir.swiechowski@gmail.com>
2020-09-15 12:23:59 +0200

    admin: setup di container

--- services/admin/client/src/App.tsx
+++ services/admin/client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button} from '@material-ui/core';
+import {Button, Container} from '@material-ui/core';
 import {
     BrowserRouter as Router,
     Switch,
@@ -10,36 +10,38 @@ import {
 function App() {
     return (
         <Router>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                    <li>
-                        <Link to="/register">Register</Link>
-                    </li>
-                    <li>
-                        <Link to="/profile">Profile</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Switch>
-                <Route path="/login">
-                    <Button variant="contained" color="primary">Login</Button>
-                </Route>
-                <Route path="/register">
-                    <Button variant="contained" color="primary">Register</Button>
-                </Route>
-                <Route path="/profile">
-                    <Button variant="contained" color="primary">Profile</Button>
-                </Route>
-                <Route path="/">
-                    <Button variant="contained" color="primary">Example button</Button>
-                </Route>
-            </Switch>
+            <Container maxWidth="lg">
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/register">Register</Link>
+                        </li>
+                        <li>
+                            <Link to="/profile">Profile</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/login">
+                        <Button variant="contained" color="primary">Login</Button>
+                    </Route>
+                    <Route path="/register">
+                        <Button variant="contained" color="primary">Register</Button>
+                    </Route>
+                    <Route path="/profile">
+                        <Button variant="contained" color="primary">Profile</Button>
+                    </Route>
+                    <Route path="/">
+                        <Button variant="contained" color="primary">Example button</Button>
+                    </Route>
+                </Switch>
+            </Container>
         </Router>
     );
 }
