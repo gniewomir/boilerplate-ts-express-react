0a27213d1a2cd1eb10d93c558c3380c3a66d302f
Gniewomir Świechowski <gniewomir.swiechowski@gmail.com>
2020-09-15 10:15:15 +0200

    api: simplify typeorm.cli.ts for readability

--- services/api/src/application/config/typeorm.cli.ts
+++ services/api/src/application/config/typeorm.cli.ts
@@ -1,23 +1,23 @@
 import config from "./index";
 
 export = (() => {
-    const connectionConfig = process.env.NODE_ENV === 'testing'
-        ? config.database.connections.testing
-        : config.database.connections.default;
 
     if (process.env.RUNNING_IN_CONTAINER !== 'true' && process.env.NODE_ENV === 'testing') {
         return {
-            ...connectionConfig,
+            ...config.database.connections.testing,
             host: 'localhost',
             port: 5433,
         }
     }
+
     if (process.env.RUNNING_IN_CONTAINER !== 'true' && process.env.NODE_ENV === 'development') {
         return {
-            ...connectionConfig,
+            ...config.database.connections.default,
             host: 'localhost',
             port: 5432,
         }
     }
-    return connectionConfig;
+
+    return process.env.NODE_ENV === 'testing' ? config.database.connections.testing : config.database.connections.default;
+
 })();
