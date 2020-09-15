e68ae0780e115f7ce9a9cc9403c909d1906c489d
Gniewomir Świechowski <gniewomir.swiechowski@gmail.com>
2020-09-15 13:01:45 +0200

    admin: switched to @reach router because of: https://reacttraining.com/blog/reach-react-router-future/

--- services/admin/client/src/component/app/App.tsx
+++ services/admin/client/src/component/app/App.tsx
@@ -1,49 +1,36 @@
 import React from 'react';
-import {Button, Container} from '@material-ui/core';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import {Container} from '@material-ui/core';
+import {Link, Router} from "@reach/router"
+import Login from "../page/Login";
+import Profile from "../page/Profile";
+import Register from "../page/Register";
+import Home from "../page/Home";
 
-function App() {
+export default () => {
     return (
-        <Router>
-            <Container maxWidth="lg">
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/login">Login</Link>
-                        </li>
-                        <li>
-                            <Link to="/register">Register</Link>
-                        </li>
-                        <li>
-                            <Link to="/profile">Profile</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route path="/login">
-                        <Button variant="contained" color="primary">Login</Button>
-                    </Route>
-                    <Route path="/register">
-                        <Button variant="contained" color="primary">Register</Button>
-                    </Route>
-                    <Route path="/profile">
-                        <Button variant="contained" color="primary">Profile</Button>
-                    </Route>
-                    <Route path="/">
-                        <Button variant="contained" color="primary">Example button</Button>
-                    </Route>
-                </Switch>
-            </Container>
-        </Router>
+        <Container maxWidth="lg">
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link to="/register">Register</Link>
+                    </li>
+                    <li>
+                        <Link to="/profile">Profile</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Router>
+                <Register path="/register"/>
+                <Login path="/login"/>
+                <Profile path="/profile"/>
+                <Home path="/"/>
+            </Router>
+        </Container>
     );
-}
-
-export default App;
+};
---
+++ services/admin/client/src/component/page/Profile.tsx
@@ -0,0 +1,7 @@
+import {Button} from "@material-ui/core";
+import React from "react";
+import {IRoutableProps} from "../../type/IRoutableProps";
+
+export default (props: IRoutableProps) => {
+    return <Button variant="contained" color="primary">Profile</Button>;
+}
