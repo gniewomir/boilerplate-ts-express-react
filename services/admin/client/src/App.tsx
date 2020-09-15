ace125646c889c023afd3caac27ef4754a6858f5
Gniewomir Świechowski <gniewomir.swiechowski@gmail.com>
2020-09-15 11:56:00 +0200

    admin: setup react router

--- services/admin/client/src/App.tsx
+++ services/admin/client/src/App.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
 import {Button} from '@material-ui/core';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
     return (
-        <Button variant="contained" color="primary">Hello World</Button>
+        <Router>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link to="/register">Register</Link>
+                    </li>
+                    <li>
+                        <Link to="/profile">Profile</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route path="/login">
+                    <Button variant="contained" color="primary">Login</Button>
+                </Route>
+                <Route path="/register">
+                    <Button variant="contained" color="primary">Register</Button>
+                </Route>
+                <Route path="/profile">
+                    <Button variant="contained" color="primary">Profile</Button>
+                </Route>
+                <Route path="/">
+                    <Button variant="contained" color="primary">home</Button>
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
