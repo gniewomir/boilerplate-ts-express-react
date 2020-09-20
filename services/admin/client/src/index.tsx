287e50059fb380d39f8f97515859dc660c81e42b
Gniewomir Świechowski <gniewomir.swiechowski@gmail.com>
2020-09-20 21:27:13 +0200

    admin: provide store and theme

--- services/admin/client/src/index.tsx
+++ services/admin/client/src/index.tsx
@@ -1,12 +1,20 @@
 import 'reflect-metadata';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './ui/app/App';
+import {Provider} from 'react-redux'
 import * as serviceWorker from './serviceWorker';
+import {store} from './state';
+import {AppConnected} from "./ui/App";
+// because: https://github.com/mui-org/material-ui/issues/13394
+import {ThemeProvider, unstable_createMuiStrictModeTheme as createMuiTheme} from '@material-ui/core/styles';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+        <Provider store={store}>
+            <ThemeProvider theme={createMuiTheme()}>
+                <AppConnected/>
+            </ThemeProvider>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
