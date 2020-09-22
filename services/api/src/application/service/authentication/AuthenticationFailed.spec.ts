48ad703231a04a1270f5aef7238a81e579037423
Gniewomir Świechowski <gniewomir.swiechowski@gmail.com>
2020-09-22 12:48:46 +0200

    api: test authentication failed object

---
+++ services/api/src/application/service/authentication/AuthenticationFailed.spec.ts
@@ -0,0 +1,40 @@
+import {cleanupTestDatabaseConnection, setupTestApplicationUserAndAuthentication} from "../../../test/utility";
+import {AuthenticationFailed} from "./AuthenticationFailed";
+import {InvalidAuthentication} from "../../error/InvalidAuthentication";
+import {Permission} from "../../permission/Permission";
+import {IPermission} from "../../type/authorization";
+
+afterAll(cleanupTestDatabaseConnection)
+
+describe('AuthenticationFailed object', () => {
+    it('Is sealed', async () => {
+        const {authentication} = await setupTestApplicationUserAndAuthentication();
+        try {
+            // @ts-ignore
+            authentication.test = 'test';
+        } catch (error) {
+            expect(error.name).toBe('TypeError');
+        }
+    });
+    it('To throw when asked for token', async () => {
+        expect(() => {
+            const subject = new AuthenticationFailed();
+            subject.getToken();
+        }).toThrow(InvalidAuthentication);
+    })
+    it('To throw when asked for user', async () => {
+        expect(() => {
+            const subject = new AuthenticationFailed();
+            subject.getToken();
+        }).toThrow(InvalidAuthentication);
+    })
+    it('Deny permissions', async () => {
+        class TestPermission extends Permission implements IPermission {
+        }
+        expect((new AuthenticationFailed()).granted(new TestPermission())).toBe(false);
+        expect((new AuthenticationFailed()).denied(new TestPermission())).toBe(true);
+    })
+    it('Deny authentication', async () => {
+        expect((new AuthenticationFailed()).isAuthenticated()).toBe(false);
+    })
+});
